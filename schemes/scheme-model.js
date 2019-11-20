0cbf4ea5a3f56070e37796defc0e00becb03adf4
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-11-19 20:31:56 -0700

    added dbconfig and started helpers

---
+++ schemes/scheme-model.js
@@ -0,0 +1,35 @@
+const db = require("../data/dbconfig.js");
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes").where({ id });
+}
+
+function findSteps(id) {
+  return db
+    .select("*")
+    .from("steps")
+    .join("schemes", "steps.scheme_id", "schemes.id")
+    .where({scheme_id: id})
+    .orderBy("steps.step_number");
+}
+
+function add(scheme) {
+  return db
+    .insert(scheme)
+    .into("schemes")
+    .then(res => {
+      const id = res[0];
+      return db("schemes").where({ id });
+    });
+}
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add
+};
